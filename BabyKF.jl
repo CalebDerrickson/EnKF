124dc9b807692b04d2871bdcd75e2696a7ed93cf
Caleb Derrickson <Calebderrickson@gmail.com>
2025-04-07 16:00:45 -0500

    error in sizes. ask phil (or vishwas)

--- BabyKF.jl
+++ BabyKF.jl
@@ -19,22 +19,27 @@ function BabyKF(xf, y, H, R, infl, rho, observe_index, localize=false)
         zb = observable
         zbm = mean(zb, dims=2) 
         Zb = (1 / sqrt(N - 1)) * (zb .- repeat(zbm, 1, N))
-        
+
         K = (rhoHPHt .* (Zb * Zb') + R) \ inn
         xa = xf + rhoPH .* (xf_dev * Zb') * K
 
     else
         # Implement VecchiaMLE here. H is the linear mapping from state space to observation locations (xyGrid -> ptGrid). 
         #n = cld(sqrt(N)), k = 10, samples = ensemble (xf), number_of_samples, ptGrid  
-        ptGrid = VecchiaMLE.generate_safe_xyGrid(N)
-        println("ptGrid size: ", size(ptGrid))
-        obs_pts = ptGrid[observe_index]
-        println("N: ", N)
-        println("observable size: ", size(observable))
-        println("obs_pts size: ", size(obs_pts))
         zb = Matrix{Float64}((observable .- mean(observable, dims=2))')
-        input = VecchiaMLEInput(N, 10, zb, size(zb, 1), 5, 1; ptGrid=ptGrid, observed_pts=obs_pts)
+        n = Int(sqrt(size(zb, 2)))
+        Number_of_Samples = size(zb, 1)
+
+
+        ptGrid = VecchiaMLE.generate_safe_xyGrid(n)
+        println("ptGrid size: ", size(ptGrid))
+        
+        println("N: ", N)
+        println("zb size: ", size(zb))
+        
+        input = VecchiaMLEInput(n, 10, zb, Number_of_Samples, 5, 1; ptGrid=ptGrid)
         _, L = VecchiaMLE_Run(input)
+
         L_inv = inv(L)
 
         println("R size: ", size(R))
@@ -42,11 +47,19 @@ function BabyKF(xf, y, H, R, infl, rho, observe_index, localize=false)
         println("L size: ", size(L))
         println("xf_dev size: ", size(xf_dev))
 
-        K = (L_inv' * L_inv + R) \ inn
+        pt1 = L_inv' * L_inv
+        pt2 = pt1 + R
+        K = pt2 \ inn 
         println("K size: ", size(K))
         println("\n")
 
-        xa = xf + (xf_dev * L_inv') * K
+        t1 = xf_dev
+        t2 = L_inv' * K
+        println("t1 size: ", size(t1))
+        println("t2 size: ", size(t2))
+
+
+        xa = xf + t1 * t2
     end
     
 
