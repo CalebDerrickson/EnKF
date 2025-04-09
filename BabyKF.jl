904514f631eadef055a53a8bac401aabede92f6e
Caleb Derrickson <Calebderrickson@gmail.com>
2025-04-09 13:24:38 -0500

    got something that kinda works, but with nneighbors really small (k = 1 or 2 for reasonable rmse). ask vishwas

--- BabyKF.jl
+++ BabyKF.jl
@@ -5,9 +5,9 @@ function BabyKF(xf, y, H, R, infl, rho, observe_index::AbstractVector, localize=
     # Since Julia 0.7 sqrt of a matrix is sqrtm in Matlab
     y_perturbed = repeat(y, 1, N) + sqrt.(R) * randn(num_observation, N)
 
-    xfm = mean(xf, dims=2)
-    xf_dev = (1 / sqrt(N-1)) * (xf - repeat(xfm, 1, N))
-    xf = sqrt(N - 1) * infl * xf_dev + repeat(xfm, 1, N)
+    #xfm = mean(xf, dims=2)
+    #xf_dev = (1 / sqrt(N-1)) * (xf - repeat(xfm, 1, N))
+    #xf = sqrt(N - 1) * infl * xf_dev + repeat(xfm, 1, N)
 
     observable = H * xf
     inn = y_perturbed - observable
@@ -32,72 +32,54 @@ function BabyKF(xf, y, H, R, infl, rho, observe_index::AbstractVector, localize=
 
     else
         verbose=false
-        # Implement VecchiaMLE here. H is the linear mapping from state space to observation locations (xyGrid -> ptGrid). 
-        #n = cld(sqrt(N)), k = 10, samples = ensemble (xf), number_of_samples, ptGrid  
-        xf_zeroed = Matrix{Float64}((xf .- mean(xf, dims=2))')
-        n = Int(sqrt(size(xf_zeroed, 2)))
-        Number_of_Samples = size(xf_zeroed, 1)
+
+        xf_mat = Matrix{Float64}(xf')
+        n = Int(sqrt(size(xf_mat, 2)))
+        Number_of_Samples = size(xf_mat, 1)
 
 
         ptGrid = VecchiaMLE.generate_safe_xyGrid(n)
-        input = VecchiaMLEInput(n, 10, xf_zeroed, Number_of_Samples, 5, 1; ptGrid=ptGrid)
+        input = VecchiaMLEInput(n, 4, xf_mat, Number_of_Samples, 5, 1; ptGrid=ptGrid)
         _, L = VecchiaMLE_Run(input)
         L_inv = inv(L)
-        diag_R = view(R, diagind(R))
-
+        R_inv = inv(R)
+        #println("L cond: ", cond(L))
 
         temp = zeros(N^2, N^2)
-        fill!(view(temp, observe_index, observe_index), 1.0) 
-        view(temp, observe_index, observe_index) ./= diag_R
+        t1 = H' * R_inv * H
+        #view(temp, observe_index, observe_index) .= R_inv
+
 
         # Inner part of the Kalman filter product
         # I.e., ( Rₖ - Hₖ(Bₖ⁻¹ + HₖᵀRₖ⁻¹Hₖ)⁻¹Hₖᵀ )
         # The inv() is really ugly, and needs to be changed.
-        inner = view(inv(L*L' + temp), observe_index, observe_index)
+        inner = H * inv(L*L' + t1) * H'
         inner .= R - inner
+        #println("inner cond after inverse: ", cond(inner))
         
         # outer part of Kalman filter product
         # I.e., BₖHₖᵀRₖ⁻¹(inner)Rₖ⁻¹
-        inner ./= R
-        inner ./= R # twice from both sides?
+        inner = R_inv * inner * R_inv
+
+        # check condition number of inner
+        #println("inner cond: ", cond(inner))
 
         # Now need to map from 100 x 100 to 2500 x 100. 
-        # is there a better way?
         outer = view(temp, :, observe_index)
         fill!(outer, 0.0)
 
-        println("inner size: ", size(inner))
-        println("N: ", N)
-        println("outer size: ", size(outer))
-        println("observe_index : ", size(observe_index))
         view(outer, observe_index, :) .= inner
 
-        lmul!(L_inv, outer)
-        lmul!(l_inv', outer)
+        outer = L_inv * outer 
+        outer = L_inv' * outer 
+        
         K = outer
-
+        #println("K cond: ", cond(K))
 
         # now to apply the Kalman filter
-        verbose = true
-        verbose && println("xf_zeroed size: ", size(xf_zeroed))
-        verbose && println("K size: ", size(K))
-        verbose && println("xf_dev size: ", size(xf_dev))
-        verbose && println("xf size: ", size(xf))
+        xa = xf + K * inn
 
-        t1 = xf_zeroed * K
-        t2 = xf_dev * t1
-        xa = xf + t2
-
-
-        verbose && println("ptGrid size: ", size(ptGrid))
-        verbose && println("N: ", N)
         
-        verbose && println("R size: ", size(R))
-        verbose && println("inn size: ", size(inn))
-        verbose && println("L size: ", size(L))
-        verbose && println("\n")
-        verbose && println("t1 size: ", size(t1))
-        verbose && println("t2 size: ", size(t2))
     end
     
 
